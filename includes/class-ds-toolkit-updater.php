daa2f41bef8f9ff2ab9e20ef42298c2357a85e9c
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 04:29:27 +0800

    Add native WP updater - no external library

--- includes/class-ds-toolkit-updater.php
+++ includes/class-ds-toolkit-updater.php
@@ -2,18 +2,89 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class DS_Toolkit_Updater {
-    public function init() {
-        $puc_path = DS_TOOLKIT_PATH . 'vendor/yahnis-elsts/plugin-update-checker/load-v5p6.php';
-        if ( ! file_exists( $puc_path ) ) {
-            return;
-        }
-        require_once $puc_path;
 
-        $updateChecker = YahnisElsts\PluginUpdateChecker\v5p6\PucFactory::buildUpdateChecker(
-            'https://github.com/agabriel1590/ds-toolkit/',
-            DS_TOOLKIT_PATH . 'ds-toolkit.php',
-            'ds-toolkit'
+    private $slug    = 'ds-toolkit';
+    private $repo    = 'agabriel1590/ds-toolkit';
+    private $api_url = 'https://api.github.com/repos/agabriel1590/ds-toolkit/releases/latest';
+
+    public function init() {
+        add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'check_for_update' ) );
+        add_filter( 'plugins_api', array( $this, 'plugin_info' ), 10, 3 );
+    }
+
+    public function check_for_update( $transient ) {
+        if ( empty( $transient->checked ) ) {
+            return $transient;
+        }
+
+        $release = $this->get_latest_release();
+        if ( ! $release ) {
+            return $transient;
+        }
+
+        $latest_version  = ltrim( $release['tag_name'], 'v' );
+        $current_version = DS_TOOLKIT_VERSION;
+        $plugin_file     = $this->slug . '/' . $this->slug . '.php';
+
+        if ( version_compare( $latest_version, $current_version, '>' ) ) {
+            $transient->response[ $plugin_file ] = (object) array(
+                'slug'        => $this->slug,
+                'plugin'      => $plugin_file,
+                'new_version' => $latest_version,
+                'url'         => 'https://github.com/' . $this->repo,
+                'package'     => $release['zipball_url'],
+            );
+        }
+
+        return $transient;
+    }
+
+    public function plugin_info( $result, $action, $args ) {
+        if ( $action !== 'plugin_information' || $args->slug !== $this->slug ) {
+            return $result;
+        }
+
+        $release = $this->get_latest_release();
+        if ( ! $release ) {
+            return $result;
+        }
+
+        return (object) array(
+            'name'          => 'DS Toolkit',
+            'slug'          => $this->slug,
+            'version'       => ltrim( $release['tag_name'], 'v' ),
+            'author'        => 'Alipio Gabriel',
+            'homepage'      => 'https://github.com/' . $this->repo,
+            'sections'      => array(
+                'description' => 'Design Shop custom features and build toolkit.',
+                'changelog'   => isset( $release['body'] ) ? $release['body'] : '',
+            ),
+            'download_link' => $release['zipball_url'],
         );
-        $updateChecker->setBranch( 'main' );
+    }
+
+    private function get_latest_release() {
+        $cache_key = 'ds_toolkit_latest_release';
+        $cached    = get_transient( $cache_key );
+        if ( $cached ) {
+            return $cached;
+        }
+
+        $response = wp_remote_get( $this->api_url, array(
+            'headers' => array( 'Accept' => 'application/vnd.github+json' ),
+            'timeout' => 10,
+        ) );
+
+        if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) !== 200 ) {
+            return false;
+        }
+
+        $data = json_decode( wp_remote_retrieve_body( $response ), true );
+        if ( empty( $data['tag_name'] ) ) {
+            return false;
+        }
+
+        set_transient( $cache_key, $data, 12 * HOUR_IN_SECONDS );
+        return $data;
     }
 }
