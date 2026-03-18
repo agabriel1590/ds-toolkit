990bc47c48228039834beb8555ed975e763b38fc
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 04:04:51 +0800

    Load updater in core class

--- includes/class-ds-toolkit.php
+++ includes/class-ds-toolkit.php
@@ -6,6 +6,11 @@ class DS_Toolkit {
         require_once DS_TOOLKIT_PATH . 'admin/class-ds-toolkit-admin.php';
         $admin = new DS_Toolkit_Admin();
         $admin->init();
+
+        require_once DS_TOOLKIT_PATH . 'includes/class-ds-toolkit-updater.php';
+        $updater = new DS_Toolkit_Updater();
+        $updater->init();
+
         $settings = get_option( 'ds_toolkit_settings', array() );
         if ( ! empty( $settings['enable_login_branding'] ) ) {
             require_once DS_TOOLKIT_PATH . 'features/class-ds-login-branding.php';
