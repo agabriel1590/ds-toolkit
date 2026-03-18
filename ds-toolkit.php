322bd03b7ef61213b7806f0bbceaffb3fb63675d
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 04:32:08 +0800

    v0.4.2 - enable login branding by default on activation

--- ds-toolkit.php
+++ ds-toolkit.php
@@ -3,7 +3,7 @@
  * Plugin Name: DS Toolkit
  * Plugin URI:  https://github.com/agabriel1590/ds-toolkit
  * Description: Design Shop custom features and build toolkit.
- * Version:     0.4.1
+ * Version:     0.4.2
  * Author:      Alipio Gabriel
  * Author URI:  https://github.com/agabriel1590
  * Text Domain: ds-toolkit
@@ -12,11 +12,22 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'DS_TOOLKIT_VERSION', '0.4.1' );
+define( 'DS_TOOLKIT_VERSION', '0.4.2' );
 define( 'DS_TOOLKIT_PATH', plugin_dir_path( __FILE__ ) );
 define( 'DS_TOOLKIT_URL', plugin_dir_url( __FILE__ ) );
 
 require_once DS_TOOLKIT_PATH . 'includes/class-ds-toolkit.php';
+
+register_activation_hook( __FILE__, 'ds_toolkit_activate' );
+if ( ! function_exists( 'ds_toolkit_activate' ) ) {
+    function ds_toolkit_activate() {
+        $settings = get_option( 'ds_toolkit_settings', array() );
+        if ( ! isset( $settings['enable_login_branding'] ) ) {
+            $settings['enable_login_branding'] = 1;
+            update_option( 'ds_toolkit_settings', $settings );
+        }
+    }
+}
 
 if ( ! function_exists( 'ds_toolkit_run' ) ) {
     function ds_toolkit_run() {
