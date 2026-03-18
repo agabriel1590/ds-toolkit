c031ff6064513009a3fb2c99936a93779e3ab677
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 04:17:40 +0800

    Fix: wrap ds_toolkit_run in function_exists to prevent redeclaration fatal error

--- ds-toolkit.php
+++ ds-toolkit.php
@@ -3,7 +3,7 @@
  * Plugin Name: DS Toolkit
  * Plugin URI:  https://github.com/agabriel1590/ds-toolkit
  * Description: Design Shop custom features and build toolkit.
- * Version:     0.3.1
+ * Version:     0.3.2
  * Author:      Alipio Gabriel
  * Author URI:  https://github.com/agabriel1590
  * Text Domain: ds-toolkit
@@ -12,14 +12,16 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'DS_TOOLKIT_VERSION', '0.3.1' );
+define( 'DS_TOOLKIT_VERSION', '0.3.2' );
 define( 'DS_TOOLKIT_PATH', plugin_dir_path( __FILE__ ) );
 define( 'DS_TOOLKIT_URL', plugin_dir_url( __FILE__ ) );
 
 require_once DS_TOOLKIT_PATH . 'includes/class-ds-toolkit.php';
 
-function ds_toolkit_run() {
-    $plugin = new DS_Toolkit();
-    $plugin->run();
+if ( ! function_exists( 'ds_toolkit_run' ) ) {
+    function ds_toolkit_run() {
+        $plugin = new DS_Toolkit();
+        $plugin->run();
+    }
+    add_action( 'plugins_loaded', 'ds_toolkit_run' );
 }
-add_action( 'plugins_loaded', 'ds_toolkit_run' );
