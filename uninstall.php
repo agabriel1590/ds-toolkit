a9824dd28e4bd8735daa2e77ba6057821c810046
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 03:57:41 +0800

    Clean up shop_name key on uninstall

--- uninstall.php
+++ uninstall.php
@@ -1,3 +1,10 @@
 <?php
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
-delete_option( 'ds_toolkit_settings' );
+
+$opts = get_option( 'ds_toolkit_settings', array() );
+unset( $opts['shop_name'] );
+if ( empty( $opts ) ) {
+    delete_option( 'ds_toolkit_settings' );
+} else {
+    update_option( 'ds_toolkit_settings', $opts );
+}
