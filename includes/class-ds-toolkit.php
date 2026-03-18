74093a30904e3e30833131065b88f922afe77471
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 03:36:02 +0800

    Load login branding conditionally

--- includes/class-ds-toolkit.php
+++ includes/class-ds-toolkit.php
@@ -6,5 +6,11 @@ class DS_Toolkit {
         require_once DS_TOOLKIT_PATH . 'admin/class-ds-toolkit-admin.php';
         $admin = new DS_Toolkit_Admin();
         $admin->init();
+        $settings = get_option( 'ds_toolkit_settings', array() );
+        if ( ! empty( $settings['enable_login_branding'] ) ) {
+            require_once DS_TOOLKIT_PATH . 'features/class-ds-login-branding.php';
+            $login = new DS_Login_Branding();
+            $login->init();
+        }
     }
 }
