3b87d827b75348b00ab68736eee4a3aad6ecb1b9
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 03:27:04 +0800

    Add ds-toolkit.php

---
+++ ds-toolkit.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: DS Toolkit
+ * Plugin URI:  https://github.com/agabriel1590/ds-toolkit
+ * Description: Design Shop custom features and build toolkit.
+ * Version:     0.1.0
+ * Author:      Alipio Gabriel
+ * Text Domain: ds-toolkit
+ * License:     GPL-2.0+
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'DS_TOOLKIT_VERSION', '0.1.0' );
+define( 'DS_TOOLKIT_PATH', plugin_dir_path( __FILE__ ) );
+define( 'DS_TOOLKIT_URL', plugin_dir_url( __FILE__ ) );
+
+require_once DS_TOOLKIT_PATH . 'includes/class-ds-toolkit.php';
+
+function ds_toolkit_run() {
+    $plugin = new DS_Toolkit();
+    $plugin->run();
+}
+add_action( 'plugins_loaded', 'ds_toolkit_run' );
