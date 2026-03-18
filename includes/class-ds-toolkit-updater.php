508bca95391c15d330eac50f4dde37949187cf6b
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 04:11:07 +0800

    Fix updater: use load-v5p6.php for correct PUC bootstrapping

--- includes/class-ds-toolkit-updater.php
+++ includes/class-ds-toolkit-updater.php
@@ -3,8 +3,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class DS_Toolkit_Updater {
     public function init() {
-        require_once DS_TOOLKIT_PATH . 'vendor/yahnis-elsts/plugin-update-checker/plugin-update-checker.php';
-        $updateChecker = YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
+        $puc_path = DS_TOOLKIT_PATH . 'vendor/yahnis-elsts/plugin-update-checker/load-v5p6.php';
+        if ( ! file_exists( $puc_path ) ) {
+            return;
+        }
+        require_once $puc_path;
+
+        $updateChecker = YahnisElsts\PluginUpdateChecker\v5p6\PucFactory::buildUpdateChecker(
             'https://github.com/agabriel1590/ds-toolkit/',
             DS_TOOLKIT_PATH . 'ds-toolkit.php',
             'ds-toolkit'
