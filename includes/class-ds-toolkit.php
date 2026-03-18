ce9837f3302df6e3996eceae880a3b70cf300eba
Alipio Gabriel <agabriel1590@users.noreply.github.com>
2026-03-19 04:24:52 +0800

    Remove auto-updater from core class

--- includes/class-ds-toolkit.php
+++ includes/class-ds-toolkit.php
@@ -7,10 +7,6 @@ class DS_Toolkit {
         $admin = new DS_Toolkit_Admin();
         $admin->init();
 
-        require_once DS_TOOLKIT_PATH . 'includes/class-ds-toolkit-updater.php';
-        $updater = new DS_Toolkit_Updater();
-        $updater->init();
-
         $settings = get_option( 'ds_toolkit_settings', array() );
         if ( ! empty( $settings['enable_login_branding'] ) ) {
             require_once DS_TOOLKIT_PATH . 'features/class-ds-login-branding.php';
